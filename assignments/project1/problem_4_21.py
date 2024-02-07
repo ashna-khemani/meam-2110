5b1b339a16abb8f0b85fdfd038fa556b1c460764
ashna-khemani <ashna.khemani@gmail.com>
2024-02-07 18:41:44 -0500

    4.18 and 4.21 - v1
    Ran and it seemed to work. Not sure what it is looking for. Will submit to autograder

--- assignments/project1/problem_4_21.py
+++ assignments/project1/problem_4_21.py
@@ -20,24 +20,26 @@ q = np.array([q_A, q_B, q_C])
 # Add three joints connecting N, A, B, C. How are q_A_, q_B, q_C defined in the problem?
 # Be sure to add them in the same order: q_A, q_B, q_C.
 # YOUR CODE GOES HERE
-system.AddJoint(PARENT, CHILD, JointType.rotation, UnitVector.z)
-system.AddJoint(PARENT, CHILD, JointType.rotation, UnitVector.z)
-system.AddJoint(PARENT, CHILD, JointType.rotation, UnitVector.z)
+system.AddJoint(N, A, JointType.rotation, UnitVector.z)
+system.AddJoint(A, B, JointType.rotation, UnitVector.z)
+system.AddJoint(B, C, JointType.rotation, UnitVector.z)
 
 L_A = 1
 L_B = 2
 L_C = 2
 L_N = 1
 
+from sympy import sin, cos
+
 # In A coordinates, r_Ao_Bo is easy to define
 r_Ao_Bo_A = np.array([L_A, 0, 0])
-r_Ao_Bo_N = # YOUR CODE HERE!
+r_Ao_Bo_N = np.array([L_A*cos(q_A), L_A*sin(q_A), 0])
 
-r_Bo_Bc_N = # YOUR CODE HERE!
+r_Bo_Bc_N = np.array([L_B*cos(q_B), L_B*sin(q_B), 0])
 
-r_Bc_Co_N = # YOUR CODE HERE!
+r_Bc_Co_N = np.array([-L_C*cos(q_C), -L_C*sin(q_C), 0])
 
-r_Co_Ao_N = # YOUR CODE HERE!
+r_Co_Ao_N = np.array([0, -L_N, 0])
 
 # The loop, r_Ao_Ao_N (starting at A_o and returning back to the same point) can be calculated as below
 # Your challenge will be to determine the four vectors below.
